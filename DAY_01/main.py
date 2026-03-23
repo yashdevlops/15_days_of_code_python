e0f3ec4be008044c7065215594f97b52d12a08ad
yashuu135 <yashdeo01@gmail.com>
2026-03-24 02:26:26 +0530

    Day 1: Added Python basics and notes

---
+++ DAY_01/main.py
@@ -0,0 +1,40 @@
+# Start Printing :
+print("Hello World")
+print(5)
+#Modules and pip
+import pandas  # This is an example of external module
+import hashlib  # This is an example of built in module
+print("Hi")
+# Our first code
+print("Hello World", 7)
+print(5)
+print("Bye")
+print(17*13)
+# Single-line comment
+print("Hello World")  # Inline comment
+"""
+This is a multi-line comment
+using triple quotes
+"""
+print("He said \"Hello\"")
+print("Line1\nLine2")
+print("Hello", "World", sep="-", end="!")
+print("Hello", "World", sep="-", end="!")
+# variables and data types
+a = complex(8, 2)
+b = True
+c = "Harry"
+d = None
+print(a)
+print(b)
+a1 = 9
+print(a + a1)
+print("The type of a is ", type(a))
+print("The type of b is ", type(b))
+print("The type of c is ", type(c))
+list1 = [8, 2.3, [-4, 5], ["apple", "banana"]]
+print(list1)
+tuple1 = (("parrot", "sparrow"), ("Lion", "Tiger"))
+print(tuple1)
+dict1 = {"name":"Sakshi", "age":20, "canVote":True}
+print(dict1)
